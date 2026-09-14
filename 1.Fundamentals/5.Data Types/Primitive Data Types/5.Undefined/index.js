3e4179929cc4a9e5595868a167257408d8356dab
Akki <asaikumarreddy.201it101@nitk.edu.in>
2026-09-14 19:29:28 +0530

    JS Notes

---
+++ 1.Fundamentals/5.Data Types/Primitive Data Types/5.Undefined/index.js
@@ -0,0 +1,13 @@
+let x ;     // variable declared but not initialized
+console.log(x); // Output: undefined
+
+
+function dosomething() {
+        // no return statement, so the function returns undefined
+}
+console.log(dosomething());  // Output: undefined
+
+
+let obj = {};
+console.log(obj.property);  // Output: undefined
+
