3d2d0b83182d64a48885dc7d54efd9a38c89c196
Akki <asaikumarreddy.201it101@nitk.edu.in>
2026-09-14 19:18:46 +0530

    JS Notes

---
+++ 1.Fundamentals/5.Data Types/Primitive Data Types/4.Null/index.js
@@ -0,0 +1,15 @@
+let age = null;
+console.log(age);
+
+// Examples
+
+const a1 = null;
+if(a1) {
+    console.log('a1 is not null');
+}
+else {
+    console.log('a1 is null');
+}
+// since null boolean value is false , else statement will come as output
+
+
